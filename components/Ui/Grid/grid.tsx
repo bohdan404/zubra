f132fa7fa24b5ae043f64833e145de5495183a86
bohdan <78599673+bohdan404@users.noreply.github.com>
2022-07-17 13:12:08 +0300

    Delete components directory

--- components/Ui/Grid/grid.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactElement } from "react";
-import s from "./grid.module.css";
-
-type Props = {
-  children: ReactElement;
-};
-export default function Grid({ children }: Props) {
-  return <div className={s.grid}>{children}</div>;
-}
